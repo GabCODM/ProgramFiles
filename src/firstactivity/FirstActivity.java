c520783282913200fca529e693772d3afe64d7f4
ACER <ACER@DESKTOP-8JDS5U4>
2024-08-04 19:46:49 +0800

    BASTA AMO LA ITU

---
+++ src/firstactivity/FirstActivity.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstactivity;
+
+/**
+ *
+ * @author ACER
+ */
+public class FirstActivity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LogIn Login = new LogIn();
+        Login.setVisible(true);
+        Login.setLocationRelativeTo(null);
+//          String str = "Hello World";
+//          String result = str.substring(0, 5);
+//          
+//          System.out.println(result);
+    }
+    
+}
